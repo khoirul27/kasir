09b08108d578eb9dd67ae065f5497ca61b081b5e
pStar7 <132860482+khoirul27@users.noreply.github.com>
2023-12-08 13:54:37 +0700

    tes

--- application/views/template.php
+++ application/views/template.php
@@ -1,4 +1,3 @@
-<!-- jhsajd -->
 <!DOCTYPE html>
 <html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default" data-assets-path="<?= base_url('assets') ?>/assets/" data-template="vertical-menu-template-free">
 
